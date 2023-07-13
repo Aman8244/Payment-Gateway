06b2fa7e9a7e1f662d54d84680e9cc8fccf0ca4e
Aman Singh <amansinghmarch16@gmail.com>
2023-07-13 15:57:26 +0530

    updated

--- DBconfig/DBconfig.js
+++ DBconfig/DBconfig.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const DbConfig = async()=>{
-    await mongoose.connect("http://mongdb://127.0.0.1/socialmediamarketing")
-    .then(res=>{
-        console.log("mongodb connected");
-    }).catch(err=>{
-        console.log("Error: ",err)
-    })
-}   
-export default DbConfig;
+// const DbConfig = async()=>{
+//     await mongoose.connect("http://mongdb://127.0.0.1/socialmediamarketing")
+//     .then(res=>{
+//         console.log("mongodb connected");
+//     }).catch(err=>{
+//         console.log("Error: ",err)
+//     })
+// }   
+// export default DbConfig;
